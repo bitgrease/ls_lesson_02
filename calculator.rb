46862c249c060ba8dcc1aad1df259837856f8cf3
Kelly Netterville <bitgrease@gmail.com>
2017-07-10 20:52:40 -0400

    Updated to only convert in actual math operation and used methods as do_math() args instead of getting that info on separate lines.

--- calculator.rb
+++ calculator.rb
@@ -9,7 +9,7 @@ end
 def get_number
   entered_number = nil
   loop do
-    say "Enter the a number: "
+    say "Enter a number: "
     entered_number = gets.chomp
     # Float will catch both Integer and Floats
     break if Float(entered_number) rescue false
@@ -33,29 +33,24 @@ end
 def do_math(num1, num2, operator)
   # Decide if floats are entered or appropriate in case of division
   if num1.include?('.') || num2.include?('.') || operator == "3"
-    num1_converted = num1.to_f
-    num2_converted = num2.to_f
+    to_method = 'to_f'
   else
-    num1_converted = num1.to_i
-    num2_converted = num2.to_i
+    to_method = 'to_i'
   end
   case operator
-  when '1' then say "#{num1_converted_converted} + #{num2_converted} = " \
-    + "#{num1_converted + num2_converted}."
-  when '2' then say "#{num1_converted} - #{num2_converted} = " \
-    + "#{num1_converted - num2_converted}."
-  when '3' then say "#{num1_converted} / #{num2_converted} = " \
-    + "#{num1_converted / num2_converted}."
-  when '4' then say "#{num1_converted} * #{num2_converted} = " \
-    + "#{num1_converted * num2_converted}."
+  when '1' then say "#{num1} + #{num2} = " \
+    + "#{num1.send(to_method) + num2.send(to_method)}."
+  when '2' then say "#{num1} - #{num2} = " \
+    + "#{num1.send(to_method) - num2.send(to_method)}."
+  when '3' then say "#{num1} / #{num2} = " \
+    + "#{num1.send(to_method) / num2.send(to_method)}."
+  when '4' then say "#{num1} * #{num2} = " \
+    + "#{num1.send(to_method) * num2.send(to_method)}."
   end
 end
 
 loop do
-  first_number = get_number
-  second_number = get_number
-  operation = get_operator
-  do_math(first_number,second_number,operation)
+  do_math(get_number,get_number,get_operator)
   puts "Do another operation?(y/n):"
   break if gets.chomp.downcase == 'n'
 end
