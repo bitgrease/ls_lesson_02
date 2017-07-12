52686829cbb17c322c734b11c29f27afd899d605
Kelly Netterville <bitgrease@gmail.com>
2017-07-12 11:55:54 -0400

    Updated calculator to use a yml file for messages.

--- calculator.rb
+++ calculator.rb
@@ -1,58 +1,53 @@
+require 'yaml'
 puts '===================================='
 puts '=            Ruby Calculator       ='
 puts '===================================='
-
+MESSAGES = YAML.load_file('calculator_messages.yml')
 def say(msg)
   puts "=> #{msg}"
 end
 
 def operator_to_message(op)
-  op_text case op
-          when '1' then 'Adding'
-          when '2' then 'Subtracting'
-          when '3' then 'Dividing'
-          when '4' then 'Multiplying'
-          end
+  op_text = case op
+            when '1' then 'Adding'
+            when '2' then 'Subtracting'
+            when '3' then 'Dividing'
+            when '4' then 'Multiplying'
+            end
   op_text
 end
 
 def get_name
   loop do
-    say "Welcome to Calculator! Enter your name: "
+    say MESSAGES['welcome']
     name = gets.chomp
     if name.match?(/^[a-zA-Z]+$/)
       return name
     end
-    say "Please enter a valid name (letters only)."
+    say MESSAGES['valid_message']
   end
 end
 
 def get_number
   entered_number = nil
   loop do
-    say 'Enter a number: '
+    say MESSAGES['enter_number']
     entered_number = gets.chomp
     # Allow entry of integers or floats
     break if entered_number.match?(/^(\+|\-){0,1}\d+\.{0,1}\d*$/)
-    say 'You must enter an integer or a float!'
+    say MESSAGES['valid_number']
   end
   entered_number
 end
 
 def get_operator
   operation = nil
-  operator_prompt = <<-MSG
-    Enter your desired operation:
-    1) Add
-    2) Subtract
-    3) Divide
-    4) Multiply
-  MSG
+  operator_prompt = MESSAGES['prompt_for_operator']
   loop do
     say operator_prompt
     operation = gets.chomp
     break if %w[1 2 3 4].include? operation
-    say 'You must enter the number for the operation - 1,2,3,or 4.'
+    say MESSAGES['valid_operation']
   end
   operation
 end
@@ -87,7 +82,7 @@ name = get_name
 say "Alright #{name}, let's do math!"
 loop do
   do_math(get_number, get_number, get_operator)
-  say 'Do another operation?(y/n):'
+  say MESSAGES['another_operation?']
   break if gets.chomp.casecmp('n').zero?
 end
 
