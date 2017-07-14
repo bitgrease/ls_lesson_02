128b005c1ebf41b6f3325735685a4b6d6eb6f802
Kelly Netterville <bitgrease@gmail.com>
2017-07-14 16:20:17 -0400

    Added scorekeeping for tournament play to the game.

--- prsls.rb
+++ prsls.rb
@@ -37,6 +37,12 @@ def win?(computer_choice, user_choice)
   WINS_AGAINST[computer_choice].include? user_choice
 end
 
+def overall_winner?(wins_count)
+  wins_count.eql? 5
+end
+
+total_computer_wins = 0
+total_player_wins = 0
 user_choice = nil
 computer_win = nil
 system 'clear'
@@ -50,13 +56,31 @@ loop do
             + "#{user_choice.capitalize}.", false)
 
   if computer_win == TIE
+    # don't count ties in overall wins
     use_prompt("That's a TIE!!")
   elsif computer_win
     use_prompt("Sorry, you lost!", false)
+    total_computer_wins += 1
   else
     use_prompt("Congrats! You WON!!!", false)
+    total_player_wins += 1
   end
-  use_prompt("Play again?(Y/N)")
+
+  use_prompt("Computer: #{total_computer_wins} - Player: #{total_player_wins}")
+  if overall_winner?(total_computer_wins)
+    puts 'Computer won the tournament ' \
+         + "#{total_computer_wins}/#{total_player_wins}."
+  elsif overall_winner?(total_computer_wins)
+    puts 'You won the tournament ' \
+         + "#{total_player_wins}/#{total_computer_wins}."
+  end
+
+  if overall_winner?(total_computer_wins) ||
+     overall_winner?(total_player_wins)
+    use_prompt("Play a new tournament?(Y/N)")
+  else
+    use_prompt("Play again?(Y/N)")
+  end
   break unless gets.chomp.downcase.start_with? 'y'
   system 'clear'
   system 'cls'
